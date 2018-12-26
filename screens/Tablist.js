190bb949ce6e1777993c6289f4785efb3d0b5401
unknown <vasilis.litsas@architechts.gr>
2018-12-26 15:54:44 +0200

    firestore get tablist

--- screens/Tablist.js
+++ screens/Tablist.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-import { Dimensions, Button, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Dimensions, Button, StyleSheet, Text, View } from 'react-native';
 import firebase from 'react-native-firebase'
 
 export default class Tablist extends Component {
+  constructor() {
+    super();
+    this.ref = firebase.firestore().collection('tabs');
+    this.unsubscribe = null;
+    this.state = {
+      textInput: '',
+      loading: true,
+      tabs: [],
+    };
+  }
+
   static navigationOptions = ({ navigation }) => {
     return {
       headerTitle: 'Tablist',
@@ -16,14 +27,73 @@ export default class Tablist extends Component {
   };
 
   componentDidMount() {
-    const serverTime = firebase.database().getServerTime();
-    alert(serverTime);
+    // this.ref.add({
+    //   title: 'Trooper'
+    // });
+
+    // this.getTabs();
+
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate)
+
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const tabs = [];
+    querySnapshot.forEach((doc) => {
+      const { title } = doc.data();
+      tabs.push({
+        key: doc.id,
+        doc, // DocumentSnapshot
+        title
+      });
+    });
+    this.setState({
+      tabs,
+      loading: false
+    });
+  }
+
+  getTabs = () => {
+
+    const tabs = [];
+
+    this.ref.get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          console.log('No matching documents.');
+          return;
+        }
+
+        snapshot.forEach(doc => {
+          const { title } = doc.data();
+          tabs.push({
+            key: doc.id,
+            title
+          })
+
+        });
+
+        this.setState({ tabs })
+
+      })
+      .catch(err => {
+        console.log('Error getting documents', err);
+      });
+
   }
 
   render() {
     return (
       <View style={styles.container}>
         {/* <Text>Tablist</Text> */}
+        <FlatList
+          data={this.state.tabs}
+          renderItem={({ item }) => <Text key={item.id}>{item.title}</Text>}
+        />
       </View>
     );
   }
